87cc673a43a6005d3bee4c9722c403da98756c4e
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2024-07-08 15:21:52 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    module:  { 
+        rules: 
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader' 
+                
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader' 
+                ]
+            }
+        ]
+    },
+    plugins: 
+    [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        },
+}
